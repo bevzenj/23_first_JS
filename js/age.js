e61921f0fefd4537454f8b72fba28c0690b63f0b
bevzenj <brisk0110@gmail.com>
2021-06-18 04:04:03 +0300

    upgrate age js, added second &

--- js/age.js
+++ js/age.js
@@ -1,7 +1,8 @@
 const thisYear = 2021;
-let userYear=prompt('Введите год вашего рождения', '');
+let userYear = prompt('Введите год вашего рождения', '');
 let result = thisYear - userYear;
-if (result >= 21 & result < 25) {
+
+if (result >= 21 && result < 25) {
     alert(`Вам: ${result} года`);
 } else {
     alert(`Вам: ${result} лет`)
